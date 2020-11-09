9dad6a01c37e0faf01644bd379c919f65110cea0
Gavin Calkins <gavin.calkins@gmail.com>
2020-11-08 17:49:33 -0800

    dynamic textarea css working

--- Develop/JS/javascript.js
+++ Develop/JS/javascript.js
@@ -1,22 +1,45 @@
 // First we create a display variable that is tied to the currentDay element of the jumbotron
 var timeDisplayEl = document.getElementById("dateTime");
 
-// To dynamically display the current day, date and/or time we must create a function tied to moment.js which is pertextareaed at a recoccuring interval to reference, update and display day, date, and/or time
+// To dynamically display the current day, date and/or time we must create a function tied to moment.js which is performed at a recoccuring interval to reference, update and display day, date, and/or time
 setInterval(() => {
 
     // Within this function we must link the function to moment.js with a variable
     var currentTime = moment();
 
-    // Then we must instruct this function how to textareaat/present currentTime
-    var textareaatCurrentTime = currentTime.textareaat("dddd, D MMMM YYYY,  h:mm:ss a");
+    // Then we must instruct this function how to format/present currentTime
+    var formatCurrentTime = currentTime.format("dddd, D MMMM YYYY,  h:mm:ss a");
     
     // Then we need to insruct the function to modify the text of the current day section to actually display the day/time
-    dateTime.textContent = textareaatCurrentTime;
+    dateTime.textContent = formatCurrentTime;
 
-    // Finally at the tail end of this function, included in the setInterval wrapper, we specficy the interval that this task will be pertextareaed. In this case, it's once every 1000 milliseconds
+    // Finally at the tail end of this function, included in the setInterval wrapper, we specficy the interval that this task will be performed. In this case, it's once every 1000 milliseconds
 }, 1000);
 
-// var present = new Date().getHours();
+// Create a function to apply the past, present, and future css classes
+// In this function we will use current time to rehiterate the currentTime variable (since it wasn't defined globally)
+function applyPastPresentFuture(currentTime=moment()){
 
-// for (i = 9)
+    // Create a variable which grabs the current hour
+    var currentHour = currentTime.hour();
+    
+    // Create a for loop to loop through the textarea elements
+    for (i = 8; i<=18; i++){
 
+        if (currentHour>i){
+            
+            $("#userInput"+i).addClass("past");
+        } 
+        
+        else if (currentHour ==i){
+            
+            $("#userInput" + i).addClass("present");
+        }
+        
+        else if (currentHour<i){
+            
+            $("#userInput" + i).addClass("future");
+        }
+    }
+}
+applyPastPresentFuture();
